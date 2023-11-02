8460105fc4956726c46fb4b519357f5c86d26436
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-02 14:00:34 +0100

    skrivit kod som importerar ord från ordlista.csv och lägger dem i en array

--- genereraHemligtOrd.js
+++ genereraHemligtOrd.js
@@ -1,2 +1,14 @@
+import { readFileSync } from 'node:fs'
+
+let hemligaOrden = readFileSync("data/ordlista.csv", "utf8")
+
+hemligaOrden = hemligaOrden.trim().split("\r\n")
+
+let ordListan = []
+
+for (let orden of hemligaOrden) {
+  ordListan.push(orden)
+}
+
 /* här ska du skriva kod som generar hemligt ord från ordlistan
 kommer motsvara skriva in ord att gissa i förra hangman*/
