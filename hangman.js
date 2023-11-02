9c3799039697122df7a22eb984c934eefda3dcfa
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-02 11:45:36 +0100

    ändrat koden i json-filen att inḱludera prompt-sync

--- hangman.js
+++ hangman.js
@@ -1,7 +1,33 @@
-import { appendFileSync, readFileSync } from 'node:fs'
+import { readFileSync } from 'node:fs'
+import PromptSync from 'prompt-sync'
+import User from "./user.js";
 
-appendFileSync('data', 'here is some content for the data file\n', 'utf-8')
+const prompt = PromptSync({ sigint: true })
 
-const data = readFileSync('data', 'utf-8')
+let dbUsers = readFileSync("data/users.csv", "utf8")
 
-console.log(data) 
+dbUsers = dbUsers.trim().split("\r\n")
+
+let users = []
+
+for (let dbUser of dbUsers) {
+  dbUser = dbUser.split(",")
+  users.push(new User(dbUser[0], dbUser[1]))
+}
+
+let user
+
+const username = prompt("please enter your username: ")
+const password = prompt("please enter your password: ")
+
+for (let savedUser of users) {
+  if (savedUser.checkCredentials(username, password)) {
+    user = savedUser
+  }
+}
+
+if (user) {
+  console.log("Welcome, " + user.name)
+} else {
+  console.log("couldn't find a user with the given username and password combo")
+}
