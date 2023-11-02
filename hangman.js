6722049a954c29c24519da2e5c90fed5ce1fcfa3
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-02 13:29:37 +0100

    lagt till hangman.js och lagt in lite kod

--- hangman.js
+++ hangman.js
@@ -1,33 +1,4 @@
-import { readFileSync } from 'node:fs'
-import PromptSync from 'prompt-sync'
-import User from "./user.js";
+import Game from "./game.js"
 
-const prompt = PromptSync({ sigint: true })
+new Game()
 
-let dbUsers = readFileSync("data/users.csv", "utf8")
-
-dbUsers = dbUsers.trim().split("\r\n")
-
-let users = []
-
-for (let dbUser of dbUsers) {
-  dbUser = dbUser.split(",")
-  users.push(new User(dbUser[0], dbUser[1]))
-}
-
-let user
-
-const username = prompt("please enter your username: ")
-const password = prompt("please enter your password: ")
-
-for (let savedUser of users) {
-  if (savedUser.checkCredentials(username, password)) {
-    user = savedUser
-  }
-}
-
-if (user) {
-  console.log("Welcome, " + user.name)
-} else {
-  console.log("couldn't find a user with the given username and password combo")
-}
