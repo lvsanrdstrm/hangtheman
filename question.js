9db9a95028c7b21ea36ef14633c95008438e1824
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-02 14:40:18 +0100

    skrivit kod som kollar att användarens input är bara bokstäver och bara en, och som kollar om den gissat på dne bokstaven förut

--- question.js
+++ question.js
@@ -5,10 +5,38 @@ const prompt = require('prompt-sync')({ sigint: true })
 export default class Question {
 
   constructor(questionText) {
-    this.questionText = questionText
+    this.questionText = this.askQuestion()
     /* ersatt detta från original
     this.answer = prompt(questionText).toUpperCase() */
-    this.answer = this.askQuestion()
+  }
+
+  askQuestion() {
+    let guessedLetter
+    do {
+      guessedLetter = prompt(this.questionText).trim().toUpperCase()
+      if (!this.isOnlyLetters(guessedLetter)) {
+        console.log("Please enter letters only.")
+      }
+      if (!this.isOnlyOneLetter(guessedLetter)) {
+        print("Please enter only one letter.")
+      }
+    } while (!this.isOnlyLetters(guessedLetter) && !this.isOnlyOneLetter(guessedLetter))
+    if (!this.usedLetters.includes(guessedLetter)) {
+      print('You already tried this letter, you moron')
+    }
+    this.usedLetters.push(guessedLetter)
+    print('You have now tried the following letters: ' + this.usedLetters)
+    return guessedLetter
+  }
+
+  isOnlyLetters(userInput) {
+    // Check if userInput is only letters
+    return /^[A-Ö]+$/.test(userInput) // +:et gör att den testar för mer än ett tecken. [A-Ö] kollar att det är ETT tecken mellan de två
+  }
+
+  isOnlyOneLetter(userInput) {
+    // Check if userInput is only letters
+    return /^[A-Ö]$/.test(userInput) // +:et gör att den testar för mer än ett tecken. [A-Ö] kollar att det är ETT tecken mellan de två
   }
 
 }
