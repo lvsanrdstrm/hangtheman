3de16e70a225ce38cee42c23d827b20dd1b827fd
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-06 10:38:40 +0100

    lagt in found-word-kod

--- found-word.js
+++ found-word.js
@@ -2,7 +2,23 @@ export default class FoundWord {
 
   letters = []
 
+  get asString() {
+    return this.letters.join(" ")
+  }
+
   constructor(secretWord) {
     this.createNotFoundLetters(secretWord.chars)
+  }
+
+  createNotFoundLetters(chars) {
+    for (let letter of chars) {
+      this.letters.push("*")
+    }
+  }
+
+  applyFoundLetter(letter, positions) {
+    for (let index of positions) {
+      this.letters[index] = letter
+    }
   }
 }
